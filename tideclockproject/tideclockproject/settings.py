b6b695839e68b87b48519d4c14b08942db49f3ab
David Scorer <davis1x@Davids-MacBook-Pro.local>
2013-10-23 20:57:57 +0100

    Added clock face to index.html

--- tideclockproject/tideclockproject/settings.py
+++ tideclockproject/tideclockproject/settings.py
@@ -49,7 +49,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH, "static")
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -57,6 +57,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(PROJECT_PATH, "static"),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
